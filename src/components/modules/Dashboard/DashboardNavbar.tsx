cbc991dc2fd23035bacc2377a047e791f2b88e89
jahidul islam <jahidul20673@gmail.com>
2026-09-23 19:12:48 +0600

    fixing the mobile view and primary color

--- src/components/modules/Dashboard/DashboardNavbar.tsx
+++ src/components/modules/Dashboard/DashboardNavbar.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { getDefaultDashboardRoute } from '@/lib/authUtils';
+import { getNavItemsByRole } from '@/lib/navItems';
+import { getUserInfo } from '@/services/auth.services';
+import { NavSection } from '@/types/dashboard.type';
+import DashboardNavbarContent from './DashboardNavbarContent';
 
-const DashboardNavbar = () => {
+const DashboardNavbar = async () => {
+    const userInfo = await getUserInfo();
+    const navItems: NavSection[] = getNavItemsByRole(userInfo.role);
+    const dashboardHome = getDefaultDashboardRoute(userInfo.role);
+
     return (
-        <div>DashboardNavbar</div>
+        <DashboardNavbarContent dashboardHome={dashboardHome} navItems={navItems} userInfo={userInfo} />
     )
 }
 
