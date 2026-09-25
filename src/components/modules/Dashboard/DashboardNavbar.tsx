9dfeb18b48ce90392cb254e9e72d22501603d6e2
jahidul islam <jahidul20673@gmail.com>
2026-09-25 10:40:59 +0600

    formatting some file

--- src/components/modules/Dashboard/DashboardNavbar.tsx
+++ src/components/modules/Dashboard/DashboardNavbar.tsx
@@ -1,17 +1,21 @@
-import { getDefaultDashboardRoute } from '@/lib/authUtils';
-import { getNavItemsByRole } from '@/lib/navItems';
-import { getUserInfo } from '@/services/auth.services';
-import { NavSection } from '@/types/dashboard.type';
-import DashboardNavbarContent from './DashboardNavbarContent';
+import { getDefaultDashboardRoute } from "@/lib/authUtils";
+import { getNavItemsByRole } from "@/lib/navItems";
+import { getUserInfo } from "@/services/auth.services";
+import { NavSection } from "@/types/dashboard.type";
+import DashboardNavbarContent from "./DashboardNavbarContent";
 
 const DashboardNavbar = async () => {
-    const userInfo = await getUserInfo();
-    const navItems: NavSection[] = getNavItemsByRole(userInfo.role);
-    const dashboardHome = getDefaultDashboardRoute(userInfo.role);
+  const userInfo = await getUserInfo();
+  const navItems: NavSection[] = getNavItemsByRole(userInfo.role);
+  const dashboardHome = getDefaultDashboardRoute(userInfo.role);
 
-    return (
-        <DashboardNavbarContent dashboardHome={dashboardHome} navItems={navItems} userInfo={userInfo} />
-    )
-}
+  return (
+    <DashboardNavbarContent
+      dashboardHome={dashboardHome}
+      navItems={navItems}
+      userInfo={userInfo}
+    />
+  );
+};
 
-export default DashboardNavbar
+export default DashboardNavbar;
